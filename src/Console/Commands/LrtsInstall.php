5ed710372d5f44cf5598fe6961383303c6eb26c0
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-10-16 01:00:12 +0800

    update vendors

---
+++ src/Console/Commands/LrtsInstall.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Iqbalfarhan\Lrtsengine\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class LrtsInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lrts:install';
+
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate React view structure under resources/js/pages/{name}';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // IqbaFarhan/LrtsEngine
+        Artisan::call('vendor:publish', [
+          '--provider' => 'Iqbalfarhan\Lrtsengine\Providers\LrtsEngineServiceProvider',
+          '--tag' => 'lrtsengine-config'
+        ]);
+
+        Artisan::call('vendor:publish', [
+          '--provider' => 'Iqbalfarhan\Lrtsengine\Providers\LrtsEngineServiceProvider',
+          '--tag' => 'lrtsengine-resources'
+        ]);
+
+        // Spatie/MediaLibrary
+
+        Artisan::call('vendor:publish', [
+          '--provider' => 'Spatie\MediaLibrary\MediaLibraryServiceProvider',
+          '--tag' => 'medialibrary-migrations'
+        ]);
+
+        Artisan::call('vendor:publish', [
+          '--provider' => 'Spatie\MediaLibrary\MediaLibraryServiceProvider',
+          '--tag' => 'medialibrary-config'
+        ]);
+
+        // Spatie/Permission
+        Artisan::call('vendor:publish', [
+          '--provider' => 'Spatie\Permission\PermissionServiceProvider',
+        ]);
+    }
+    
+}
